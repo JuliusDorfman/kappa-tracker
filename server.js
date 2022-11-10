7d1b2bc86a3a0958a09297ef4a2e424c06dc189f
JuliusDorfman <juliusgcd@gmail.com>
2022-11-09 16:47:49 -0800

    backend and frontend set up individually

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require ('path');
+const chalk = require('chalk');
+// const connectDB = require(path.resolve(__dirname, 'server', 'config', 'mongoDB'));
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// connectDB();
+
+
+require('dotenv').config({path: path.join(__dirname, '/')});
+// require('dotenv').config({path: path.join(__dirname, 'server' ,'/')});
+
+
+
+// ROUTES
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+const port = process.env.PORT || 7000;
+
+// app.use('/api', require(path.resolve(__dirname, 'server', 'gamesRoutes.js'), next =>{
+	// res.header("Access-Control-Allow-Origin", "https://art-of-twitch.herokuapp.com/"); // update to match the domain you will make the request from
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	res.set('Content-Type', 'application/json');
+// }));
+// });
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'build')));
+	// app.get('*', (req, res) => {
+	app.get('/*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	});
+}
+
+app.listen(port, () => console.log(chalk.blueBright(`Server started on port ${port}. Listening...`)));
+
+// Handle Routes 
